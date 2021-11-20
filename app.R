fdfc2fe5f4d789ac9e21fb7876175b03b43bccde
Marwin <marwinbr@gmail.com>
2021-11-20 11:19:11 -0300

    dashboard layout

--- app.R
+++ app.R
@@ -1,72 +1,99 @@
-library(shiny)
-library(shinyWidgets)
-library(leaflet)
-library(dplyr)
+## pacotes ----
+suppressPackageStartupMessages({
+  library(shiny)
+  library(shinyWidgets)
+  library(shinyjs)
+  library(shinydashboard)
+  library(leaflet)
+  library(dplyr)
+  library(ggplot2)
+})
+
+## database ----
 
 base <- readr::read_rds("data/cnpq_completo.rds")
 
-ui <- fluidPage(
-  
-  tabPanel("Interactive map",
-           div(class="outer",
-               
-               tags$head(
-                 # Include our custom CSS
-                 includeCSS("styles.css"),
-                 #includeScript("gomap.js")
-               ),
-               
-               # If not using custom CSS, set height of leafletOutput to a number instead of percent
-               leafletOutput("map", width="100%", height="100%"),
-               
-               # Shiny versions prior to 0.11 should use class = "modal" instead.
-               absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
-                             draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-                             width = 330, height = "auto",
-                             
-                             h2("Mapa das bolsas"),
-                             
-                             sliderInput(
-                               inputId = "ano_bolsa",
-                               label = "Escolha um período:",
-                               sep = "",
-                               step = 1,
-                               min = min(base$ano_referencia),
-                               max = max(base$ano_referencia),
-                               value = c(2016,2020)
-                             ),
-                             
-                             selectInput(inputId = "categoria_bolsa", 
-                                         label = "Categoria da bolsa",
-                                         choices = sort(unique(base$categoria)),
-                                         multiple = TRUE),
-                             
-                             selectInput(inputId = "modalidade_bolsa", 
-                                         label = "Modalidade da bolsa",
-                                         choices = c("Carregando..." = ""),
-                                         selectize = TRUE,
-                                         multiple = TRUE),
-                             
-                             selectInput(inputId = "area_bolsa", 
-                                         label = "Grande Área",
-                                         choices = sort(unique(base$grande_area)),
-                                         multiple = TRUE),
-                             
-                             p("Este mapa mostra a distribuição das cidades de destino
+## Tabs ----
+
+#source("ui/map_tab.R")
+source("ui/sidebar.R")
+
+
+ui <- dashboardPage(
+  skin = 'blue',
+  dashboardHeader(title = 'Bolsas CNPq'),
+  sidebar = sidebar,
+  dashboardBody(
+    tabItems(
+      tabItem(
+        tabName = "map_tab",
+        fluidPage(
+          tabPanel("",
+                   div(class="outer",
+                       
+                       tags$head(
+                         # Include our custom CSS
+                         includeCSS("styles.css"),
+                         #includeScript("gomap.js")
+                       ),
+                       
+                       # If not using custom CSS, set height of leafletOutput to a number instead of percent
+                       leafletOutput("map", width="100%", height="100%"),
+                       
+                       # Shiny versions prior to 0.11 should use class = "modal" instead.
+                       absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                                     draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                                     width = 330, height = "auto",
+                                     
+                                     h2("Mapa das bolsas"),
+                                     
+                                     sliderInput(
+                                       inputId = "ano_bolsa",
+                                       label = "Escolha um período:",
+                                       sep = "",
+                                       step = 1,
+                                       min = min(base$ano_referencia),
+                                       max = max(base$ano_referencia),
+                                       value = c(2016,2020)
+                                     ),
+                                     
+                                     selectInput(inputId = "categoria_bolsa", 
+                                                 label = "Categoria da bolsa",
+                                                 choices = sort(unique(base$categoria)),
+                                                 multiple = TRUE),
+                                     
+                                     selectInput(inputId = "modalidade_bolsa", 
+                                                 label = "Modalidade da bolsa",
+                                                 choices = c("Carregando..." = ""),
+                                                 selectize = TRUE,
+                                                 multiple = TRUE),
+                                     
+                                     selectInput(inputId = "area_bolsa", 
+                                                 label = "Grande Área",
+                                                 choices = sort(unique(base$grande_area)),
+                                                 multiple = TRUE),
+                                     
+                                     p("Este mapa mostra a distribuição das cidades de destino
                                de bolsas cedidas pelo cnpq. Utilizando os campos acima,
                                você pode filtrar os resultados selecionando um ou mais
                                critérios específicos. Caso nenhum filtro esteja selecionado, 
                                serão apresentados os resultados para todas as categorias 
                                e modalidades."),
-                             p("Ao aproximar a visão do mapa, um marcador indicará cada
+                               p("Ao aproximar a visão do mapa, um marcador indicará cada
                                cidade para qual houve a destinação de alguma bolsa.
                                Ao clicar no marcador, você poderá ver o nome da cidade
-                               e a quantidade de bolsas cedidas.")
-                             
-                             
-                             )))
-
+                               e a quantidade de bolsas cedidas."),
+                               p("Adaptado de https://github.com/rstudio/shiny-examples/tree/main/063-superzip-example")
+                               
+                       )
+                   )
+          )
+          
+        )
+      )
+    )
   )
+)
 
 server <- function(input, output, session) {
   
---
+++ ui/sidebar.R
@@ -0,0 +1,12 @@
+sidebar <- dashboardSidebar(
+  collapsed = FALSE,
+  # https://fontawesome.com/icons?d=gallery&m=free
+  sidebarMenu(
+    id = "tabs",
+    # menuItem("Main", tabName = "main_tab",
+    #          icon = icon("home")),
+    menuItem("Mapa", tabName = "map_tab",
+             icon = icon("home"),
+             startExpanded = TRUE)
+  )
+)
